e7dec32008836e89bd7a8a6040263c972351f4a5
Allen <allenhe77@gmail.com>
2020-10-14 20:32:02 +0300

    setup home and register route

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ app = express();
 
 const PORT = 5000;
 
+app.get("/", (req, res) => {
+  res.send("you are at backend root");
+});
+
+app.post("/register", (req, res) => {
+  res.send("registration recived!");
+});
+
 app.listen(PORT, (req, res) => {
   console.log(`listening on port ${PORT}`);
 });
