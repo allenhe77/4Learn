e49cb7dad775c8827a6c57f197b3f86a84f9f69b
Allen <allenhe77@gmail.com>
2020-10-14 22:19:52 +0300

    home page and a bit of styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+
+import "./App.css";
+import Home from "./Components/Home";
+import Register from "./Components/Register";
 
 function App() {
   return (
-  <div>new app</div>
-    );
+    <div>
+      <div className="container-topnav">
+        <nav>
+          <ul className="top-nav">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/dashboard">
+              <li>Dashboard</li>
+            </Link>
+            <Link to="/askquestion">
+              <li>Ask Question</li>
+            </Link>
+
+            <Link to="/answerquestion">
+              <li>Answer Question</li>
+            </Link>
+            <Link to="/register">
+              <li>Register</li>
+            </Link>
+            <Link to="/Login">
+              <li>Login</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route path exact="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
