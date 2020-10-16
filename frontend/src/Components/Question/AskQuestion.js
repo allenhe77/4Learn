62ca790509c9262a4969449097540f75d9a8d267
Allen <allenhe77@gmail.com>
2020-10-16 14:15:21 +0300

    insert q for backend and frontend

---
+++ frontend/src/Components/Question/AskQuestion.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AskQuestion = () => {
+  return (
+    <div>
+      <form action="/askquestion" method="POST">
+        <label for="title">Question Title</label>
+        <input type="text" name="title"></input>
+        <label for="area">Question Area</label>
+        <input type="text" name="area"></input> <br />
+        <textarea placeholder="enter your question" name="detail"></textarea>
+        <button type="submit" name="submit">
+          Submit Question
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AskQuestion;
