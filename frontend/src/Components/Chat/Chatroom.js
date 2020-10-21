4e781c907b484910c68e0cce9e4f50f4f4cc5d3a
Allen <allenhe77@gmail.com>
2020-10-21 15:36:03 +0300

    initial working module for chatroom

--- frontend/src/Components/Chat/Chatroom.js
+++ frontend/src/Components/Chat/Chatroom.js
@@ -1,18 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import io from "socket.io-client";
+import Message from "./Message";
 
 const Chatroom = () => {
+  const [message, setMessage] = useState([]);
+  const [click, setClicked] = useState(true);
+  // const [socket, setSocket] = useState(0);
+  const socket = useRef(0);
+  //only run once
+
   useEffect(() => {
-    const socket = io();
-    //clientmessage is the custom event
-    //socket.emit("clientmessage", { message: "hello from client" });
-    socket.on("server-message", (data) => {
-      console.log(data.message);
+    socket.current = io();
+
+    socket.current.on("server-message", (data) => {
+      setMessage((message) => [...message, data]);
     });
   }, []);
+
+  const handleClick = () => {
+    socket.current.emit("clientmessage", { message: "message from client" });
+  };
+
+  useEffect(() => {
+    // const socket = io();
+    let iniRender = true;
+    if (iniRender) {
+      iniRender = false;
+    } else {
+    }
+  }, []);
+
   return (
     <div>
-      <h1>chat room</h1>
+      <div className="container-chat">
+        {message.map((e) => (
+          <p>{e.message} </p>
+        ))}
+      </div>
+      <div className="container-chat-enter">
+        <button onClick={handleClick}>Send</button>
+      </div>
     </div>
   );
 };
