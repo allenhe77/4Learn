5d0d3634cd7f081bff6521be2cf0880978ead171
Allen <allenhe77@gmail.com>
2020-10-23 21:26:30 +0300

    parse string to jsx

--- frontend/src/Components/Question/Question.js
+++ frontend/src/Components/Question/Question.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import parse from "html-react-parser";
 const Question = (props) => {
   return (
     <div className="container-view-question">
       <h4>Title: {props.title}</h4>
       <h6>Area of Question:{props.area}</h6>
-      <p>{props.detail}</p>
+
+      <p>{parse(props.detail)}</p>
       <textarea placeholder="write your answer here"></textarea>
       <button>Submit Your Asnwer!</button>
     </div>
