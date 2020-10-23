fabfcf8f974a6f053b18ac72e20cc788bddfcc3f
Allen <allenhe77@gmail.com>
2020-10-23 21:26:11 +0300

    added image

---
+++ frontend/src/Components/HomeBody/Left.js
@@ -0,0 +1,12 @@
+import React from "react";
+import studentImage from "../../images/student1.jpg";
+
+const Left = () => {
+  return (
+    <div className="container-image">
+      <img src={studentImage}></img>
+    </div>
+  );
+};
+
+export default Left;
