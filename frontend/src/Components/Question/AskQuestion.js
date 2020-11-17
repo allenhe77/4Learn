48d89474f33a5192a040c43b249cd5fd8154e05a
Allen <allenhe77@gmail.com>
2020-11-17 11:33:09 +0300

    initial commit

--- frontend/src/Components/Question/AskQuestion.js
+++ frontend/src/Components/Question/AskQuestion.js
@@ -1,32 +1,40 @@
 import React from "react";
+import "./AskQuestion.css";
 
 const AskQuestion = () => {
   return (
-    <div>
+    <div className="ask-question">
       <form action="/adminquestion" method="POST">
-        <label for="title">Question Title</label>
-        <input type="text" name="title"></input>
+        <input
+          type="text"
+          name="title"
+          className="question-title"
+          placeholder="Your Question's Title"
+        ></input>{" "}
+        <br />
         <label for="area">Question Area</label>
         <input type="text" name="area"></input> <br />
         <textarea
           placeholder="enter your question"
           name="detail"
           id="noise"
-          className="widgEditor"
+          className="question-detail"
         ></textarea>
-        <h4>Tags</h4>
-        <label for="oalevel">O Level / Alevel</label>
-        <input name="oalevel" type="text"></input>
-        <label for="subject">Subject</label>
-        <input name="subject"></input>
-        <label for="year">Year</label>
-        <input name="year"></input>
-        <label for="paperNumber">Paper Number</label>
-        <input name="paperNumber"></input>
-        <label for="month">June / October</label>
-        <input name="month"></input>
-        <label for="questionNumber">Question Number</label>
-        <input name="questionNumber"></input> <br />
+        <div className="tags">
+          <h4>Tags</h4>
+          <label for="oalevel">O Level / Alevel</label>
+          <input name="oalevel" type="text"></input>
+          <label for="subject">Subject</label>
+          <input name="subject"></input>
+          <label for="year">Year</label>
+          <input name="year"></input>
+          <label for="paperNumber">Paper Number</label>
+          <input name="paperNumber"></input>
+          <label for="month">June / October</label>
+          <input name="month"></input>
+          <label for="questionNumber">Question Number</label>
+          <input name="questionNumber"></input> <br />
+        </div>
         <button type="submit" name="submit">
           Submit Question
         </button>
