97a9751d5e59d33e297f34c07ac249c5df672e14
Allen <allenhe77@gmail.com>
2020-11-18 12:11:59 +0300

    initial commit

--- frontend/src/Components/Question/AskQuestion.js
+++ frontend/src/Components/Question/AskQuestion.js
@@ -12,8 +12,7 @@ const AskQuestion = () => {
           placeholder="Your Question's Title"
         ></input>{" "}
         <br />
-        <label for="area">Question Area</label>
-        <input type="text" name="area"></input> <br />
+        <input type="text" name="area" className="question-area"></input> <br />
         <textarea
           placeholder="enter your question"
           name="detail"
@@ -22,20 +21,37 @@ const AskQuestion = () => {
         ></textarea>
         <div className="tags">
           <h4>Tags</h4>
-          <label for="oalevel">O Level / Alevel</label>
-          <input name="oalevel" type="text"></input>
-          <label for="subject">Subject</label>
-          <input name="subject"></input>
-          <label for="year">Year</label>
-          <input name="year"></input>
-          <label for="paperNumber">Paper Number</label>
-          <input name="paperNumber"></input>
-          <label for="month">June / October</label>
-          <input name="month"></input>
-          <label for="questionNumber">Question Number</label>
-          <input name="questionNumber"></input> <br />
+          <div className="individual-tag">
+            <label for="oalevel">O Level / Alevel</label>
+            <input name="oalevel" type="text"></input>
+          </div>
+
+          <div className="individual-tag">
+            <label for="subject">Subject</label>
+            <input name="subject"></input>
+          </div>
+
+          <div className="individual-tag">
+            <label for="year">Year</label>
+            <input name="year"></input>
+          </div>
+
+          <div className="individual-tag">
+            <label for="paperNumber">Paper Number</label>
+            <input name="paperNumber"></input>
+          </div>
+
+          <div className="individual-tag">
+            <label for="month">June / October</label>
+            <input name="month"></input>
+          </div>
+
+          <div className="individual-tag">
+            <label for="questionNumber">Question Number</label>
+            <input name="questionNumber"></input> <br />
+          </div>
         </div>
-        <button type="submit" name="submit">
+        <button type="submit" name="submit" className="submit-button">
           Submit Question
         </button>
       </form>
