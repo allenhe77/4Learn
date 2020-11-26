1b0a95e40749aa18ad4d5fb4811db9d42f0a74e9
Allen <allenhe77@gmail.com>
2020-11-26 18:47:05 +0300

    change answer modal layout

--- frontend/src/Components/Question/AnswerModal.js
+++ frontend/src/Components/Question/AnswerModal.js
@@ -5,13 +5,14 @@ const AnswerModal = (props) => {
   return (
     <div className="outer-answer">
       <dialog open>
-        <p>Question Description</p>
-
-        {Object.keys(props.tags).map((e) => (
-          <h4>
-            {e + ":"} . {props.tags[e]}
-          </h4>
-        ))}
+        <p className="question-desc">Question Description</p>
+        <div className="wrapper-questions">
+          {Object.keys(props.tags).map((e) => (
+            <h5>
+              {e + ":"} . {props.tags[e]}
+            </h5>
+          ))}
+        </div>
 
         {Object.keys(props.answer).map((e) => (
           <div className="answer">
